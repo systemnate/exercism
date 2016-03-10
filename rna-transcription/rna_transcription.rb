8829b7b71f550e183758ddb46156f7abb521c47f
Nate Dalo <nate.dalo@yahoo.com>
2016-03-10 12:04:21 -0600

    wrap long line to a new line for added clarity

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,9 +1,10 @@
 class Complement
-  VERSION = 3
+  VERSION = 4
 
   MAPPING = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
 
   def self.of_dna(dna)
-    dna.chars.map! { |c| MAPPING[c] or raise ArgumentError, "Invalid character detected! (ACGTU are valid)" }.join("")
+    dna.chars.map! { |c| MAPPING[c] or raise ArgumentError,
+       "Invalid character detected! (ACGTU are valid)" }.join("")
   end
 end
