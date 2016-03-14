4469b3498b155845b383afbaa48fa770df8bc55d
Nate Dalo <nate.dalo@yahoo.com>
2016-03-14 12:54:06 -0500

    improve 'total' method for 'grains'

--- grains/grains.rb
+++ grains/grains.rb
@@ -2,14 +2,10 @@ class Grains
   VERSION = 2
 
   def self.square(n)
-    2**(n-1)
+    2 ** ( n - 1 )
   end
 
   def self.total
-    grains = []
-    (1..64).to_a.each do |i|
-      grains << square(i)
-    end
-    grains.inject(:+)
+    2 ** 64 - 1
   end
 end
