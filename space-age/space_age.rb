d92a15910ba043802dc7da4389bffbab4e46d106
Nate Dalo <nate.dalo@yahoo.com>
2016-03-23 11:46:42 -0500

    add metaprogramming to 'space-age'

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,41 +1,14 @@
 class SpaceAge
   attr_reader :seconds
-
   EARTH_SECONDS = 31557600.0
+  MAPPING = { "earth" => 1, "mercury" => 0.2408467, "venus" => 0.61519726, "mars" => 1.8808158,
+              "jupiter" => 11.8808158, "saturn" => 29.447498, "uranus" => 84.016846, "neptune" => 164.79132  }
 
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def on_earth
-    seconds / EARTH_SECONDS
-  end
-
-  def on_mercury
-    seconds / (EARTH_SECONDS * 0.2408467)
-  end
-
-  def on_venus
-    seconds / (EARTH_SECONDS * 0.61519726)
-  end
-
-  def on_mars
-    seconds / (EARTH_SECONDS * 1.8808158)
-  end
-
-  def on_jupiter
-    seconds / (EARTH_SECONDS * 11.8808158)
-  end
-
-  def on_saturn
-    seconds / (EARTH_SECONDS * 29.447498)
-  end
-
-  def on_uranus
-    seconds / (EARTH_SECONDS * 84.016846)
-  end
-
-  def on_neptune
-    seconds / (EARTH_SECONDS * 164.79132)
+  def method_missing(method_sym, *args, &block)
+    seconds / (EARTH_SECONDS * MAPPING[method_sym.to_s.split("_")[1]])
   end
 end
