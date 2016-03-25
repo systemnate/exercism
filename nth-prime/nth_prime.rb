6171509a5b8ae4005ff0e23a5ceb1383e83db51d
Nate Dalo <nate.dalo@yahoo.com>
2016-03-25 10:25:23 -0500

    improve error message for 'nth_prime'

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,6 +1,7 @@
 class Prime
   def self.nth(n)
-    raise ArgumentError, "argument cannot be less than or = to 0" if n <= 0
+    raise ArgumentError, 
+      "n must be > 0 (#{n} for 1+)." if n <= 0
     prime_numbers = []
     i = 2
     while prime_numbers[n-1] == nil
