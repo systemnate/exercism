d5d884f754c32776618978c79b8faa85ff3ef4e6
Nate Dalo <nate.dalo@yahoo.com>
2016-03-28 11:17:06 -0500

    complete 'queen_attack'

---
+++ queen-attack/queen_attack.rb
@@ -0,0 +1,48 @@
+class Queens
+  attr_reader :opts
+
+  DEFAULT = { white: [0,3], black: [7,3] }
+
+  def initialize(opts = {})
+    @opts = DEFAULT.merge(opts)
+    raise ArgumentError if @opts[:white] == @opts[:black]
+  end
+
+  def white
+    opts[:white]
+  end
+
+  def black
+    opts[:black]
+  end
+
+  def to_s
+    str = ""
+    array.each do |a|
+      str << a.join.strip + "\n"
+    end
+    str.chomp
+  end
+
+  def attack?
+    (attack_line? or attack_diagonal?) ? true : false
+  end
+
+  private
+
+  def array
+    arr = []
+    8.times { arr << Array.new(8, "_ ") }
+    arr[white.first][white.last] = "W "
+    arr[black.first][black.last] = "B "
+    arr
+  end
+
+  def attack_diagonal?
+    (white.first - black.first).abs == (white.last - black.last).abs
+  end
+
+  def attack_line?
+    white[0] == black[0] or white[1] == black[1] ? true : false
+  end
+end
